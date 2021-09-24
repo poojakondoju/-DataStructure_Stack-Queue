ecf5ee1f5ba926e841333ae94c9f2446a226d0dd
poojakondoju <kondojupoojitha612@gmail.com>
2021-09-24 10:13:18 +0530

    Added : Program for Create queqe and add element

--- src/com/bridgelabz/Operation.java
+++ src/com/bridgelabz/Operation.java
@@ -36,4 +36,9 @@ public class Operation {
 		stacklist.display();
 		stacklist.peek();
 	}
+
+	public static void dequeueElement() {
+		Operation.queueElement();
+		Operation.pop();
+	}
 }
--- src/com/bridgelabz/StackQueqeMain.java
+++ src/com/bridgelabz/StackQueqeMain.java
@@ -10,6 +10,7 @@ public class StackQueqeMain {
 		System.out.println("Press '2' to peek the element");
 		System.out.println("Press '3' to pop the element");
 		System.out.println("Press '4' to add element in the Queqe");
+		System.out.println("Press '5' to dequeue element");
 		switch (sc.nextInt()) {
 		case 1:
 			Operation.addElement();
@@ -23,6 +24,9 @@ public class StackQueqeMain {
 			break;
 		case 4:
 			Operation.queueElement();
+			break;
+		case 5:
+			Operation.dequeueElement();
 			break;
 		default: {
 			System.out.println("Press Valid Key");
